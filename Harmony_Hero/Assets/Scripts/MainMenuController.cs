f9658d4b7567f2b9d356ff6549b49d57a22b0f61
school <yho45@my.centennialcollege.ca>
2025-02-09 20:47:47 -0500

    set up main menu, game over UI

---
+++ Harmony_Hero/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class MainMenuController : MonoBehaviour
+{
+    public Image cover;
+    public float delay = 1f;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        StartCoroutine(DelayedMethod(1f));
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator DelayedMethod(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        cover.enabled = false;
+    }
+}

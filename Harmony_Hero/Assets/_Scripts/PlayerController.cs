0a45c387c248b7a5eae1e5e838f1aaddbbae60c0
school <yho45@my.centennialcollege.ca>
2025-02-09 22:09:34 -0500

    map: player movement

--- Harmony_Hero/Assets/_Scripts/PlayerController.cs
+++ Harmony_Hero/Assets/_Scripts/PlayerController.cs
@@ -1,16 +1,36 @@
+using Unity.Android.Gradle.Manifest;
 using UnityEngine;
+using UnityEngine.Windows;
 
 public class PlayerController : MonoBehaviour
 {
+    private CharacterController _controller;
+    private InputSystem_Actions _inputs;
+    [SerializeField] private Vector3 _move;
+    [SerializeField] private float _velocity;
+
+    private void Awake()
+    {
+        _controller = GetComponent<CharacterController>();
+        _inputs = new InputSystem_Actions();
+
+        _inputs.Player.Move.performed += context => _move = context.ReadValue<Vector2>();
+        _inputs.Player.Move.canceled += context => _move = Vector2.zero;
+    }
+
+    private void OnEnable() => _inputs.Enable();
+    private void OnDisable() => _inputs.Disable();
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 movement = new Vector3(_move.x * _velocity * Time.fixedDeltaTime, 0.0f, _move.y * _velocity * Time.fixedDeltaTime);
+        _controller.Move(movement);
     }
 }

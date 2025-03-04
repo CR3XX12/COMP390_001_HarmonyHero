a0d44e2959030a4ae4f12e66cea0f0f3a8388c0c
school <yho45@my.centennialcollege.ca>
2025-03-03 22:46:29 -0500

    battle point only available when player beat the previous one

--- Harmony_Hero/Assets/_Scripts/BattlePathController.cs
+++ Harmony_Hero/Assets/_Scripts/BattlePathController.cs
@@ -10,6 +10,13 @@ public class BattlePathController : MonoBehaviour
     [SerializeField] public GameObject battlePath5;
     [SerializeField] public GameObject battlePath6;
 
+    [SerializeField] public BoxCollider battlePoint1;
+    [SerializeField] public BoxCollider battlePoint2;
+    [SerializeField] public BoxCollider battlePoint3;
+    [SerializeField] public BoxCollider battlePoint4;
+    [SerializeField] public BoxCollider battlePoint5;
+    [SerializeField] public BoxCollider battlePoint6;
+
     private PlayerController playerController;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -36,11 +43,23 @@ public class BattlePathController : MonoBehaviour
 
         // Store battle paths in an array for cleaner code
         GameObject[] battlePaths = { battlePath1, battlePath2, battlePath3, battlePath4, battlePath5, battlePath6 };
+        BoxCollider[] battlePoints = { battlePoint1, battlePoint2, battlePoint3, battlePoint4, battlePoint5, battlePoint6 };
 
         // Enable paths up to the battle index
         for (int i = 0; i < battlePaths.Length; i++)
         {
+            bool setActive;
+            if (i <= battleIndex)
+            {
+                setActive = true;
+            }
+            else
+            {
+                setActive = false;
+            }
+
             battlePaths[i].SetActive(i <= battleIndex);
+            battlePoints[i].enabled = setActive;
         }
     }
 

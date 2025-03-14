65c1122bf5eceb72d657b623d549d485472660db
school <yho45@my.centennialcollege.ca>
2025-03-13 23:22:04 -0400

    reminder when new battle unlocked

--- Harmony_Hero/Assets/_Scripts/InstructionController.cs
+++ Harmony_Hero/Assets/_Scripts/InstructionController.cs
@@ -1,14 +1,41 @@
 using TMPro;
 using UnityEngine;
-using UnityEngine.Windows;
 
 public class InstructionController : MonoBehaviour
 {
     public GameObject text;
     public GameObject instruction;
+    public GameObject reminder;
+
+    private DataKeeper dataKeeper;
     void Start()
     {
+        dataKeeper = Object.FindFirstObjectByType<DataKeeper>();
         instruction.SetActive(false);
+
+        switch (dataKeeper.currentBattle)
+        {
+            case 2:
+                reminder.GetComponent<TextMeshPro>().text = "The 2nd Verse Begins!\nTune your melody!";
+                break;
+            case 3:
+                reminder.GetComponent<TextMeshPro>().text = "The 3rd Chorus Awaits!\nStrike the right chord!";
+                break;
+            case 4:
+                reminder.GetComponent<TextMeshPro>().text = "The 4th Harmony Rises!\nSing your strength!";
+                break;
+            case 5:
+                reminder.GetComponent<TextMeshPro>().text = "The 5th Refrain Echoes!\nYour music guides you.";
+                break;
+            case 6:
+                reminder.GetComponent<TextMeshPro>().text = "The Final Crescendo!\nLet your song shine!";
+                break;
+            default:
+                reminder.GetComponent<TextMeshPro>().text = "Can you hear\nthe melody of fate?";
+                break;
+        }
+
+
     }
     private void Update()
     {
